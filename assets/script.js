13b7b90810f884d350c53e5593fde0156f18905d
Hayley McHugh <hayleym522@gmail.com>
2021-10-14 19:51:02 -0700

    test

--- assets/script.js
+++ assets/script.js
@@ -1,17 +1,51 @@
 var timeDisplayEL = $("#currentDay");
 
 function displayTime() {
-    var rightNow = moment().format('MMMM Do, YYYY [at] h:mm a');
-    timeDisplayEL.text(rightNow);
+var rightNow = moment().format('MMMM Do, YYYY [at] h:mm a');
+timeDisplayEL.text(rightNow);
 }
 displayTime()
 
-var text = $(".text-input")
-
-var textInput = {
-    text: text.value 
-}
-
-localStorage.setItem("textInput", (textInput));
 
 
+
+$(document).ready(function() {
+
+    $(".saveBtn").click(function() {
+        var toDoText=$(this).siblings(".toDo").val();
+        var toDoTime=$(this).parent().attr("id");
+
+        console.log(toDoText);
+        console.log(toDoTime);
+
+        localStorage.setItem(toDoTime, toDoText);
+    });
+})
+
+//     $(".row").each(function() {
+//         var hour = moment().hours();
+//         console.log(hour);
+
+//         var rowHour = parseInt($(this).attr("id"));
+
+//         console.log(localStorage.getItem(rowHour));
+//         console.log("this:", $(this).attr("id"));
+
+//         var textArea = document.querySelector(".text" + rowHour)
+
+//         // textArea.value = localStorage.getItem(rowHour)
+
+//         if (rowHour < hour) {
+//             $(this).addClass("past");
+//         }
+//         else if (rowHour === hour) {
+//             $(this).removeClass("past");
+//             $(this).addClass("present");
+//         }
+//         else {
+//             $(this).removeClass("past");
+//             $(this).removeClass("present");
+//             $(this).addClass("future");
+//         }
+//     });
+// });
